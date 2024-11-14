47d2ba94801ae363b45e2ed6d5f147a8628c0f48
Melissa <melifisher1234@gmail.com>
2024-11-14 11:32:19 -0400

    teestx3

--- test_blockchain.py
+++ test_blockchain.py
@@ -3,13 +3,13 @@ import requests
 import json
 
 def test_medical_records():
-    BASE_URL = "http://localhost:8080"
+    BASE_URL = "http://136.248.112.224:8080"
     
     # Obtener cuentas de Ganache (asumiendo que son las primeras dos)
     from web3 import Web3
     w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
     admin_address = w3.eth.accounts[0]
-    doctor_address = w3.eth.accounts[5]
+    doctor_address = w3.eth.accounts[1]
     
     # admin_private_key = w3.eth.account.from_key(w3.geth.personal.list_wallets()[0]['privateKey']).privateKey.hex()
     # doctor_private_key = '0x89a1d4aeae91f63c68c80b026ca9f7813872e79'
