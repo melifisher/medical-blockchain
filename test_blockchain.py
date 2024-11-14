fba09890f60098c1d54ac757aa01dcd7cdf29f83
Melissa <melifisher1234@gmail.com>
2024-11-13 21:42:33 -0400

    test

--- test_blockchain.py
+++ test_blockchain.py
@@ -9,12 +9,13 @@ def test_medical_records():
     from web3 import Web3
     w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
     admin_address = w3.eth.accounts[0]
-    doctor_address = w3.eth.accounts[1]
-    
+    doctor_address = w3.eth.accounts[2]
+    #0x5dC27301647601Db1d07Bd7debe2A69F9bC1a743
     # En Ganache, las llaves privadas se pueden obtener directamente
     # Nota: Esto solo funciona en entornos de desarrollo con Ganache
     # admin_private_key = w3.eth.account.from_key(w3.geth.personal.list_wallets()[0]['privateKey']).privateKey.hex()
-    doctor_private_key = '0x89a1d4aeae91f63c68c80b026ca9f7813872e79'
+    # doctor_private_key = '0x89a1d4aeae91f63c68c80b026ca9f7813872e79'
+    doctor_private_key = '0x481dfa18fe66d3cbfc33bf3ec7fe28e774318d944d39a9bbed595664c4276171'
 
     # 1. Autorizar nuevo doctor
     print("Autorizando doctor...")
