d007c755b50130e3e7ced97ccb8d1ca06ae08175
Melissa <melifisher1234@gmail.com>
2024-11-14 10:37:24 -0400

    test

--- test_blockchain.py
+++ test_blockchain.py
@@ -9,11 +9,11 @@ def test_medical_records():
     from web3 import Web3
     w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
     admin_address = w3.eth.accounts[0]
-    doctor_address = w3.eth.accounts[1]
+    doctor_address = w3.eth.accounts[5]
     
     # admin_private_key = w3.eth.account.from_key(w3.geth.personal.list_wallets()[0]['privateKey']).privateKey.hex()
     # doctor_private_key = '0x89a1d4aeae91f63c68c80b026ca9f7813872e79'
-    doctor_private_key = '0x481dfa18fe66d3cbfc33bf3ec7fe28e774318d944d39a9bbed595664c4276171'
+    doctor_private_key = '0xf836bc9d0a955396c3463b8ffab14dfd270740489954406cfadd35e73ea861c1'
 
     # 1. Autorizar nuevo doctor
     print("Autorizando doctor...")
